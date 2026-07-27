f3ab3edf67121ad619622eeba3321039cb7c6a7c
Jairo Matías <jairo_1829@hotmail.com>
2026-07-27 15:48:24 -0400

    Handle and log unhandled errors

--- src/NotepadJM/App.xaml.cs
+++ src/NotepadJM/App.xaml.cs
@@ -1,7 +1,59 @@
+using NotepadJM.Services;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace NotepadJM;
 
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        DispatcherUnhandledException += OnDispatcherUnhandledException;
+        AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
+        TaskScheduler.UnobservedTaskException += OnUnobservedTaskException;
+
+        base.OnStartup(e);
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        DispatcherUnhandledException -= OnDispatcherUnhandledException;
+        AppDomain.CurrentDomain.UnhandledException -= OnUnhandledException;
+        TaskScheduler.UnobservedTaskException -= OnUnobservedTaskException;
+
+        base.OnExit(e);
+    }
+
+    private static void OnDispatcherUnhandledException(
+        object sender,
+        DispatcherUnhandledExceptionEventArgs e)
+    {
+        AppLogger.Log(e.Exception, "Unhandled UI exception");
+
+        MessageBox.Show(
+            "Notepad JM encontró un error inesperado. El detalle fue guardado en la carpeta de registros.",
+            "Error inesperado",
+            MessageBoxButton.OK,
+            MessageBoxImage.Error);
+
+        e.Handled = true;
+    }
+
+    private static void OnUnhandledException(
+        object? sender,
+        UnhandledExceptionEventArgs e)
+    {
+        if (e.ExceptionObject is Exception exception)
+        {
+            AppLogger.Log(exception, "Unhandled application exception");
+        }
+    }
+
+    private static void OnUnobservedTaskException(
+        object? sender,
+        UnobservedTaskExceptionEventArgs e)
+    {
+        AppLogger.Log(e.Exception, "Unobserved task exception");
+        e.SetObserved();
+    }
 }
